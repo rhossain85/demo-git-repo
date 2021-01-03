854bb4a3aab2383426cd5e751b335e8a6af534d3
Rashed Hossain <rhossain85@hotmail.com>
2021-01-02 21:59:02 -0500

    Initial commit

---
+++ src/main/java/demo_git_repo/demo_git_repo/App.java
@@ -0,0 +1,13 @@
+package demo_git_repo.demo_git_repo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
